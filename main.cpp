d027af13556b72bd7c130e63066f992d00fd2222
TarikEren <1210606015@nku.edu.tr>
2024-07-24 22:11:12 +0300

    Added TODOS, removed unnecessary include

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <cstring>
 #include "headers/compiler.h"
 
 //TODO: Add a makefile for building on release.
 //TODO: Change the machine logic so that it doesn't kill the entire program when it encounters a problem.
 //TODO: Add error checking
+//      Check for value out of bounds in lexer
 
 std::vector<std::string> parse_arguments(const std::string& arguments) {
     std::vector<std::string> argument_vector{};
