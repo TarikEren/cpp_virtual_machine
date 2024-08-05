6956aad66fc32e14e5a332cfd7f10cc81b0acf53
TarikEren <1210606015@nku.edu.tr>
2024-08-06 02:58:19 +0300

    Created definitions.h

---
+++ headers/definitions.h
@@ -0,0 +1,12 @@
+#ifndef CPP_VIRTUAL_MACHINE_DEFINITIONS_H
+#define CPP_VIRTUAL_MACHINE_DEFINITIONS_H
+
+#ifndef word
+#define word uint16_t
+#endif
+
+#ifndef dword
+#define dword int32_t
+#endif
+
+#endif
