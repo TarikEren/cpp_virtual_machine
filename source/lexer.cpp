89a6fcd37c653dbf1fd3bc3fb4be4e00c8aad621
TarikEren <1210606015@nku.edu.tr>
2024-08-06 14:58:28 +0300

    Replaced string_to_hex, added new functions, added error handling

--- source/lexer.cpp
+++ source/lexer.cpp
@@ -1,6 +1,6 @@
 #include "../headers/lexer.h"
 
-Lexer::Lexer(const std::string &file_name) {
+void Lexer::read_file(const std::string& file_name) {
     std::ifstream file(file_name);
     if (!file.is_open()) {
         std::string message = "Unable to open file " + file_name;
@@ -18,46 +18,67 @@ void Lexer::advance() {
     this->current = this->text[this->index];
 }
 
-void Lexer::buffer_add() {
-    this->buffer.push_back(this->current);
-}
-
-void Lexer::buffer_clear() {
-    this->buffer.clear();
-}
-
 void Lexer::tokenize() {
     //Instruction buffer for pushing onto instructions vector.
-    std::string instruction{};
+    std::string instruction{}, operand{}, buffer{};
+    //Line and col for error handling.
+    int line = 1, col = 1;
     this->index = 0;
     this->current = this->text[this->index];
     while (current != '\0') {
         while (isalnum(current)) {
+            col += 1;
             //Push char onto buffer
-            buffer_add();
+            buffer.push_back(current);
             //Advance further
             advance();
-            if (current == '\n' || current == ' ') {
+            if (current == '\n' || current == ' ' || current == '\0') {
                 //If INT instruction has been encountered
                 if (buffer == "INT") {
+                    //If an operand wasn't provided
+                    if (!operand_exists(current)) {
+                        printf("%s Missing operand at line: %d, col: %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                     //Use the 0x5 prefix
                     instruction.append("0x5");
-                    //Clear the buffer
-                    buffer_clear();
                     //Advance further
                     advance();
                     //Until the newline character
                     while (current != '\n') {
-                        //TODO: Add checks here
+                        col += 1;
+                        //If an invalid character was provided
+                        if (!char_is_hex(current)) {
+                            printf("%s Invalid char %c at line: %d, col: %d\n", error, current, line, col);
+                            buffer.clear();
+                            this->instructions.clear();
+                            this->text.clear();
+                            return;
+                        }
                         //Push char onto buffer
-                        buffer_add();
+                        operand.push_back(current);
                         //Advance further
                         advance();
                     }
+                    line += 1;
+                    col = 1;
+                    //If the provided operand out of bounds
+                    if (!operand_in_bounds(operand)) {
+                        logger(FATAL_ERROR, "Operand " + operand + " out of bounds", OPERAND_OUT_OF_BOUNDS);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
+                    //Clear the buffer (Cleared it this late in case we need it for displaying an error message.)
+                    buffer.clear();
                     //Append the buffer onto the instruction
-                    instruction.append(buffer);
+                    instruction.append(operand);
                     //Clear the buffer
-                    buffer_clear();
+                    operand.clear();
                     //Push the instruction onto instructions
                     instructions.push_back(instruction);
                     //Clear the instruction buffer
@@ -65,66 +86,143 @@ void Lexer::tokenize() {
                 }
                     //The other keywords work the same way as explained above
                 else if (buffer == "ADD") {
+                    //If an operand wasn't provided
+                    if (!operand_exists(current)) {
+                        printf("%s Missing operand at line: %d, col: %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                     instruction.append("0x1");
-                    buffer_clear();
+                    buffer.clear();
                     advance();
                     while (current != '\n') {
-                        buffer_add();
+                        col += 1;
+                        //If an invalid character was provided
+                        if (!char_is_hex(current)) {
+                            printf("%s Invalid char %c at line: %d, col: %d\n", error, current, line, col);
+                            buffer.clear();
+                            this->instructions.clear();
+                            this->text.clear();
+                            return;
+                        }
+                        operand.push_back(current);
                         advance();
                     }
-                    instruction.append(buffer);
-                    buffer_clear();
+                    instruction.append(operand);
+                    operand.clear();
                     instructions.push_back(instruction);
                     instruction.clear();
                 }
                 else if (buffer == "SUB") {
+                    //If an operand wasn't provided
+                    if (!operand_exists(current)) {
+                        printf("%s Missing operand at line: %d, col: %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                     instruction.append("0x2");
-                    buffer_clear();
+                    buffer.clear();
                     advance();
                     while (current != '\n') {
-                        buffer_add();
+                        //If an invalid character was provided
+                        if (!char_is_hex(current)) {
+                            printf("%s Invalid char %c at line: %d, col: %d\n", error, current, line, col);
+                            buffer.clear();
+                            this->instructions.clear();
+                            this->text.clear();
+                            return;
+                        }
+                        col += 1;
+                        operand.push_back(current);
                         advance();
                     }
-                    instruction.append(buffer);
-                    buffer_clear();
+                    instruction.append(operand);
+                    operand.clear();
                     instructions.push_back(instruction);
                     instruction.clear();
                 }
                 else if (buffer == "LOAD") {
+                    //If an operand wasn't provided
+                    if (!operand_exists(current)) {
+                        printf("%s Missing operand at line: %d, col: %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                     instruction.append("0x3");
-                    buffer_clear();
+                    buffer.clear();
                     advance();
                     while (current != '\n') {
-                        buffer_add();
+                        //If an invalid character was provided
+                        if (!char_is_hex(current)) {
+                            printf("%s Invalid char %c at line: %d, col: %d\n", error, current, line, col);
+                            buffer.clear();
+                            this->instructions.clear();
+                            this->text.clear();
+                            return;
+                        }
+                        col += 1;
+                        operand.push_back(current);
                         advance();
                     }
-                    instruction.append(buffer);
-                    buffer_clear();
+                    instruction.append(operand);
+                    operand.clear();
                     instructions.push_back(instruction);
                     instruction.clear();
                 }
                 else if (buffer == "SAVE") {
+                    //If an operand wasn't provided
+                    if (!operand_exists(current)) {
+                        printf("%s Missing operand at line: %d, col: %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                     instruction.append("0x4");
-                    buffer_clear();
+                    buffer.clear();
                     advance();
                     while (current != '\n') {
-                        buffer_add();
+                        //If an invalid character was provided
+                        if (!char_is_hex(current)) {
+                            printf("%s Invalid char %c at line: %d, col: %d\n", error, current, line, col);
+                            buffer.clear();
+                            this->instructions.clear();
+                            this->text.clear();
+                            return;
+                        }
+                        col += 1;
+                        operand.push_back(current);
                         advance();
                     }
-                    instruction.append(buffer);
-                    buffer_clear();
+                    instruction.append(operand);
+                    operand.clear();
                     instructions.push_back(instruction);
                     instruction.clear();
                 }
                 else if (buffer == "PRINT") {
-                    instruction.append("0xf000");
-                    buffer_clear();
-                    instructions.push_back(instruction);
-                    instruction.clear();
+                    if (!operand_exists(current)) {
+                        instruction.append("0xf000");
+                        buffer.clear();
+                        instructions.push_back(instruction);
+                        instruction.clear();
+                    }
+                    else {
+                        printf("%s Unnecessary operand for print instruction at line %d, col %d\n", error, line, col);
+                        buffer.clear();
+                        this->instructions.clear();
+                        this->text.clear();
+                        return;
+                    }
                 }
                 else {
-                    logger(FATAL_ERROR, "Invalid instruction " + buffer, INVALID_INSTRUCTION);
-                    buffer_clear();
+                    printf("%s Invalid instruction at line %d, col %d\n", error, line, col);
+                    buffer.clear();
                     this->instructions.clear();
                     this->text.clear();
                 }
@@ -138,7 +236,43 @@ void Lexer::set_file_text(const std::string &new_text) {
     this->text = new_text;
 }
 
-char Lexer::peek() {
+char Lexer::peek(int steps) {
     if (this->current == '\0') return '\0';
-    else return this->text[this->index + 1];
+    else return this->text[this->index + steps];
+}
+
+bool Lexer::char_is_hex(char c) {
+    if ((c >= 'A' & c <= 'F') || (c >= 'a' && c <= 'f') || isdigit(c))
+        return true;
+    return false;
+}
+
+//TODO: CHECK HERE
+bool Lexer::operand_exists(char c) {
+    if (c == '\n') {
+        return false;
+    }
+    char next = this->peek(1);
+    if (isalnum(next)) return true;
+    return false;
+}
+
+word Lexer::string_to_hex(std::string& str) {
+    str = str.substr(2);
+    std::stringstream hex_stream;
+    hex_stream << std::hex << str;
+    word result;
+    hex_stream >> result;
+    return result;
+}
+
+bool Lexer::operand_in_bounds(std::string& str) {
+    std::stringstream hex_stream;
+    hex_stream << std::hex << str;
+    dword test_var;
+    hex_stream >> test_var;
+    if (test_var > 0xffff || test_var < 0) {
+        return false;
+    }
+    return true;
 }
